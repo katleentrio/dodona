61e40b474ea969074e2f886876527bdb1d6ffd24
katleentrio <161167371+katleentrio@users.noreply.github.com>
2024-04-29 15:14:26 +0200

    remove

--- Oefeningen/Selectie IF/Sneeuw/solution/solution.py
+++
@@ -1,6 +0,0 @@
-weertype = input("Welk weer is het vandaag? Je kan kiezen tussen zon, regen of sneeuw: ")
-
-if weertype == "sneeuw":
-    print("Het sneeuwt vandaag. Neem handschoenen mee.")
-
-print("Einde programma")
