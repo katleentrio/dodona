543e17383018478327f3ad44a8994d3365ec8d8d
katleentrio <161167371+katleentrio@users.noreply.github.com>
2024-04-29 11:33:22 +0200

    oef IF

---
+++ Oefeningen/IF/Sneeuw/solution/solution.py
@@ -0,0 +1,6 @@
+weertype = input("Welk weer is het vandaag? Je kan kiezen tussen zon, regen of sneeuw: ")
+
+if weertype == "sneeuw":
+    print("Het sneeuwt vandaag. Neem handschoenen mee.")
+
+print("Einde programma")
