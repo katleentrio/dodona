4f774d1b562a2d43ad7fc3b5e5dadc14c3102503
katleentrio <161167371+katleentrio@users.noreply.github.com>
2024-04-30 14:27:17 +0200

    zr

--- Oefeningen/Selectie IF ELSE/2. Regen of zon/solution/solution.py
+++ Oefeningen/Selectie IF ELSE/2. Regen of zon/solution/solution.py
@@ -1,4 +1,4 @@
-weertype = input("Welk weer is het vandaag? Je kan kiezen tussen zon, regen of sneeuw: ")
+weertype = input("Welk weer is het vandaag? Je kan kiezen tussen zon of regen: ")
 
 if weertype == "zon":
     print("De zon schijnt vandaag. Neem je zonnebril mee.")
