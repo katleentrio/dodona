d905d8ae31ab396554b1db8bb6090f23f7e1b39b
katleentrio <161167371+katleentrio@users.noreply.github.com>
2024-05-01 22:59:56 +0200

    =

--- Oefeningen/Selectie IF ELSE/5. groter of gelijk aan 100/solution/solution.py
+++ Oefeningen/Selectie IF ELSE/5. groter of gelijk aan 100/solution/solution.py
@@ -1,6 +1,6 @@
 getal = int(input("Voer een getal in: "))
 
-if getal > 100:
+if getal >= 100:
     print("Het getal is groter of gelijk aan 100.")
 else:
     print("Het getal is niet groter of gelijk aan 100")
--- Oefeningen/Selectie IF ELSE/6. 18 of ouder/solution/solution.py
+++ Oefeningen/Selectie IF ELSE/6. 18 of ouder/solution/solution.py
@@ -1,6 +1,6 @@
 leeftijd = int(input("Voer uw leeftijd in: "))
 
-if leeftijd > 18:
+if leeftijd >= 18:
     print("Toegang verleend.")
 else:
     print("Toegang geweigerd.")
