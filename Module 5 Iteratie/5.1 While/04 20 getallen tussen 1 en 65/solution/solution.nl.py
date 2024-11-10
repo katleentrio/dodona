ef02229e6e3bac1be14f3d73fa3e2d2872755331
katleentrio <161167371+katleentrio@users.noreply.github.com>
2024-11-10 22:49:09 +0100

    .

--- Module 5 Iteratie/5.1 While/04 20 getallen tussen 1 en 65/solution/solution.nl.py
+++ Module 5 Iteratie/5.1 While/04 20 getallen tussen 1 en 65/solution/solution.nl.py
@@ -6,7 +6,7 @@ kleinste = getal
 teller = 1
 
 
-while teller <= 20:
+while teller < 20:
     getal = int(input("Geef een getal: "))
     while getal < 1 or getal > 65:
         getal = int(input("Te klein of te groot. Geef een correct getal: "))
